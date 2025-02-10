3454f94156cbca6da39a44e090e65ab9df005637
Daun Kim <yennakim11@gmail.com>
2025-02-09 19:06:31 -0600

    added buttons to project cards that redirect

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,19 +12,25 @@ export default function Projects() {
       title: "Afternoon Tea",
       description: "Afternoon Tea is a frontend application built to help users keep track of different tea pairings and allow them to collectively recommend and review them.",
       image: afternoonTea,
-      tech: "JavaScript, React Bootstrap, ReactJS, Figma"
+      tech: "JavaScript, React Bootstrap, ReactJS, Figma",
+      repo: "https://github.com/yennakim/afternoon-tea",
+      demo: "https://afternoontea.netlify.app/",
     },
     {
       title: "Takeoff",
       description: "Takeoff is a Fullstack application designed to simplify travel planning and organization.",
       image: takeoff,
-      tech: "JavaScript, ReactJS, Python, Django"
+      tech: "JavaScript, ReactJS, Python, Django",
+      repo: "https://github.com/yennakim/takeoff-client",
+      demo: "Not deployed"
     },
     {
       title: "Therapy&Me",
       description: "Therapy&Me is a full-stack application designed to help individuals keep track of their therapy sessions, ensuring that no important details are forgotten.",
       image: projImg3,
-      tech: "JavaScript, ReactJS, Python, Django"
+      tech: "JavaScript, ReactJS, Python, Django",
+      repo: "Not deployed",
+      demo: "Not deployed",
     }
     
   ];
