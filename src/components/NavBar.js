1c53151ee323ee47b0a3d33f23f5ed8792483702
Daun Kim <yennakim11@gmail.com>
2025-04-13 21:19:48 -0500

    feat: resume can be viewed in a new tab/downloaded by clicking 'Resume' in the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from '../assets/img/logo.png';
 import linkedinIcon from '../assets/img/linkedin.svg';
 import githubIcon from '../assets/img/github.svg';
+import resume from '../assets/resume.pdf';
 
 export default function NavBar() {
   const [activeLink, setActiveLink] = useState('home');
@@ -46,10 +47,12 @@ export default function NavBar() {
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/daun-kim/"><img src={linkedinIcon} alt="Linkedin" /></a>
               <a href="https://github.com/yennakim/"><img src={githubIcon} alt="Github" /></a>
-
+              
             </div>
-            {/* <button className="connect" onClick={() => console.log('connect')}><span>Let's Connect</span>
-            </button> */}
+            <a href={resume} target="_blank" rel="noreferrer">
+  <button>Resume</button>
+</a>
+
           </span>
         </Navbar.Collapse>
       </Container>
