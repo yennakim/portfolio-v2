37fe0ddb22649d0e81d4f66f8e664d28724a2f23
Daun Kim <yennakim11@gmail.com>
2025-04-17 23:53:52 -0500

    updated navbar with Contact navlink

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Row, Col, Container } from 'react-bootstrap';
+
+export default function Contact() {
+
+
+  return (
+    <section className="contact" id='connect'>
+    <Container>
+      <Row className="align-items-center">
+        <Col md={6}></Col>
+      </Row>
+    </Container>
+    </section>
+  )
+}
