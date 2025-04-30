872cff069f8a8e7b7c81a9702ae0c6b741c242b6
Daun Kim <yennakim11@gmail.com>
2025-04-30 01:41:06 -0500

    fix: responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
-            <img src={logo} alt="logo" />
+            <img className="logo" src={logo} alt="logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
